1aa2fa2022b44f6ddb3f8294e0eecfed6255c88d
ahmed <ahmedenouri@gmail.com>
2025-05-18 23:17:46 +0100

    V_001

---
+++ src/main/java/ma/enset/hospitalapp/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package ma.enset.hospitalapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager(
+            User.withUsername("ahmed").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build(),
+                User.withUsername("wafaa").password(passwordEncoder().encode("1234")).roles("USER").build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin();
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+        return httpSecurity.build();
+    }
+}
